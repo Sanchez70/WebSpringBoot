c5105ceffd384c3c379bdc328fd14f453fb4cee8
Santana <carsantana54@gmail.com>
2024-01-05 11:22:03 -0500

    .

--- zHotel/src/main/java/com/zhotel/models/entity/Cantones.java
+++ zHotel/src/main/java/com/zhotel/models/entity/Cantones.java
@@ -1,32 +1,45 @@
 package com.zhotel.models.entity;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
 @Entity
-@Table(name = "cantones")
-public class Cantones {
+@Table(name="cantones", uniqueConstraints = {@UniqueConstraint(columnNames= {"id_provincia"})})
+public class Cantones implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private String id_canton;
 	private String nombre;
-	private String cod_provincia;
+	private String id_provincia;
+	
 	public String getId_canton() {
 		return id_canton;
 	}
 	public void setId_canton(String id_canton) {
 		this.id_canton = id_canton;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getCod_provincia() {
-		return cod_provincia;
+	
+	public String getId_provincia() {
+		return id_provincia;
 	}
-	public void setCod_provincia(String cod_provincia) {
-		this.cod_provincia = cod_provincia;
+	public void setId_provincia(String id_provincia) {
+		this.id_provincia = id_provincia;
 	}
 	
 	
--- zHotel/src/main/java/com/zhotel/models/entity/Provincia.java
+++ zHotel/src/main/java/com/zhotel/models/entity/Provincia.java
@@ -1,15 +1,28 @@
 package com.zhotel.models.entity;
 
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "provincias")
-public class Provincia {
+public class Provincia  implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private String id_provincia;
 	private String nombre;
+	
 	public String getId_provincia() {
 		return id_provincia;
 	}
@@ -22,5 +35,17 @@ public class Provincia {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="id_provincia")
+	private List<Cantones> cantones;
 
+	public List<Cantones> getCantones() {
+		return cantones;
+	}
+	public void setCantones(List<Cantones> cantones) {
+		this.cantones = cantones;
+	}
+	
+	
 }
